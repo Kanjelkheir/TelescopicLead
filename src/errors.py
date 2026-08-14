e8a2f037dbd2c7b3133f9e8788c8e9110424f4ba
Kanjelkheir <bilal.kanjelkheir@gmail.com>
2026-08-14 21:57:19 +0300

    Add custom error exceptions for Company class and Person class

--- src/errors.py
+++ src/errors.py
@@ -1,5 +1,18 @@
 class EmptyFirstName(Exception):
     pass
 
+
 class InvalidEmail(Exception):
+    pass
+
+
+class InvalidJobTitle(Exception):
+    pass
+
+
+class InvalidCompanyName(Exception):
+    pass
+
+
+class InvalidCompanyEmployees(Exception):
     pass
