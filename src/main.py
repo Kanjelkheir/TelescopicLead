a13470a41b6e2848731d229112ed951a97044482
Kanjelkheir <bilal.kanjelkheir@gmail.com>
2026-08-18 20:14:07 +0300

    Take job title and location arguments and run linkedin scraping

--- src/main.py
+++ src/main.py
@@ -1,5 +1,30 @@
+import argparse
+
+from src.linkedin import write_csv
+
+
 def main():
-    print("Hello from telescopiclead!")
+    parser = argparse.ArgumentParser(
+        description="Lead generation tool written in python"
+    )
+    parser.add_argument(
+        "-j",
+        "--job",
+        type=str,
+        required=True,
+        help="The job title for the target leads",
+    )
+    parser.add_argument(
+        "-l",
+        "--location",
+        type=str,
+        required=True,
+        help="The target location of the leads",
+    )
+    args = parser.parse_args()
+    job_title: str = args.job
+    location: str = args.location
+    write_csv(job_title, location)
 
 
 if __name__ == "__main__":
